87c3d5800874b1d7432cc53ffb89a157fc294811
Joshua Sim <joshuascy96@gmail.com>
2018-03-17 16:35:23 -0500

    RestaurantList troubles with Firebase

--- app/src/main/java/com/example/lawati97/deliveryapplication/RestaurantList.java
+++ app/src/main/java/com/example/lawati97/deliveryapplication/RestaurantList.java
@@ -2,12 +2,91 @@ package com.example.lawati97.deliveryapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class RestaurantList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase database;
+    DatabaseReference restaurantList;
+
+    FirebaseRecyclerAdapter<RestaurantModel,RestaurantViewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_list);
+
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        restaurantList = database.getReference("Food");
+
+        //Load restaurants
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_food);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        loadRestaurants();
+
+
+//  recyclerView = (RecyclerView)findViewById(R.id.recycler_food);
+//        recyclerView.setHasFixedSize(true);
+//        layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
     }
+
+    private void loadRestaurants() {
+        FirebaseRecyclerAdapter<RestaurantModel,RestaurantViewHolder> adapter = new FirebaseRecyclerAdapter<RestaurantModel, RestaurantViewHolder>(RestaurantModel.class,R.layout.food_menus,RestaurantViewHolder.class,restaurantList) {
+            @Override
+            protected void populateViewHolder(RestaurantViewHolder viewHolder, RestaurantModel model, int position) {
+                viewHolder.txtRestaurantName.setText(model.getName());
+                Picasso.with(getBaseContext()).load(model.getImage())
+                        .into(viewHolder.imageView);
+                final RestaurantModel clickItem = model;
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Toast.makeText(RestaurantList.this, ""+clickItem.getName(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+//    private void loadListRestaurant(){
+//        adapter = new FirebaseRecyclerAdapter<RestaurantModel, RestaurantViewHolder>(RestaurantModel.class,
+//                R.layout.food_menus,
+//                RestaurantViewHolder.class,
+//                // RestaurantList.orderByChild("MenuId")
+//                ) {
+//            @Override
+//            protected void populateViewHolder(RestaurantViewHolder viewHolder, RestaurantModel model, int position) {
+//                viewHolder.txtRestaurantName.setText(model.getName());
+//                Picasso.with(getBaseContext()).load(model.getImage())
+//                        .into(viewHolder.imageView);
+//
+//                final RestaurantModel local = model;
+//                viewHolder.setItemClickListener(new ItemClickListener() {
+//                    @Override
+//                    public void onClick(View view, int position, boolean isLongClick) {
+//                        Toast.makeText(RestaurantList.this, ""+local.getName(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//            }
+//        }
+//
+//        //Set adapter
+//        recyclerView.setAdapter(adapter);
+//    }
 }
--- app/src/main/java/com/example/lawati97/deliveryapplication/RestaurantViewHolder.java
+++ app/src/main/java/com/example/lawati97/deliveryapplication/RestaurantViewHolder.java
@@ -6,23 +6,35 @@ import android.widget.AdapterView;
 import android.widget.TextView;
 import android.widget.ImageView;
 
+import org.w3c.dom.Text;
+
 /**
  * Created by Joshua Sim on 3/16/2018.
  */
 
 public class RestaurantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    public TextView restaurant_name;
-    public ImageView restaurant_image;
+    public TextView txtRestaurantName;
+    public ImageView imageView;
 
-    private ItemClickListener itemCLickListener;
+    private ItemClickListener itemClickListener;
 
     public RestaurantViewHolder(View itemView) {
         super(itemView);
+
+        txtRestaurantName = (TextView)itemView.findViewById(R.id.restaurant_name);
+        imageView = (ImageView)itemView.findViewById(R.id.menu_image);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
     }
 
     @Override
     public void onClick(View view) {
+        itemClickListener.onClick(view,getAdapterPosition(),false);
 
     }
 }
